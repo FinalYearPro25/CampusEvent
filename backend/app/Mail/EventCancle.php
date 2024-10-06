c2cdbe8a1cfb1d673fa69b1533406f9d5436ef55
sudan94 <sudanupadhaya@gmail.com>
2024-10-06 11:35:47 +0200

    Google location autocomplete added for create events

--- backend/app/Mail/EventCancle.php
+++ backend/app/Mail/EventCancle.php
@@ -8,17 +8,40 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Headers;
+use Symfony\Component\Mime\Header\UnstructuredHeader;
+use Symfony\Component\Mime\Email;
+use Mailtrap\EmailHeader\CategoryHeader;
 
 class EventCancle extends Mailable
 {
     use Queueable, SerializesModels;
 
+    private string $name;
+    private string $location;
+    private string $title;
+    private string $startdate;
+    private string $enddate;
+    private string $description;
+    private string $sender_name;
+    private string $code;
+    private string $member_id;
     /**
      * Create a new message instance.
      */
-    public function __construct()
+    public function __construct(string $name, string $location, string $title, string $startdate, string $enddate, string $description,string $sender_name, string $code, string $member_id)
     {
-        //
+        $this->name = $name;
+        $this->location = $location;
+        $this->title = $title;
+        $this->startdate = $startdate;
+        $this->enddate = $enddate;
+        $this->description = $description;
+        $this->sender_name = $sender_name;
+        $this->code = $code;
+        $this->member_id = $member_id;
+
     }
 
     /**
@@ -27,7 +50,24 @@ class EventCancle extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
+            from : new Address(env('MAIL_FROM_ADDRESS'), $this->sender_name),
+            replyTo: [
+                new Address(env('MAIL_FROM_ADDRESS'), $this->sender_name),
+            ],
             subject: 'Event Cancle',
+            using: [
+                function (Email $email){
+                    $email->getHeaders()
+                    ->addTextHeader('X-Message-Source', env('MAIL_DOMAIN_NAME'))
+                    ->add(new UnstructuredHeader('X-Mailer', 'Mailtrap PHP Client'))
+                ;
+
+                // Category (should be only one)
+                $email->getHeaders()
+                    ->add(new CategoryHeader('Event Cancle'))
+                ;
+                }
+            ]
         );
     }
 
@@ -37,7 +77,17 @@ class EventCancle extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'mail.event-cancle',
+            with: [
+                'sender_name' => $this->sender_name,
+                'name' => $this->name,
+                'location' => $this->location,
+                'title' => $this->title,
+                'description' => $this->description,
+                'startdate' => $this->startdate,
+                'enddate' => $this->enddate,
+                'url' => env("FRONTEND_URL")."/memberEvents/".$this->member_id."/".$this->code
+            ],
         );
     }
 
@@ -49,5 +99,18 @@ class EventCancle extends Mailable
     public function attachments(): array
     {
         return [];
+    }
+    /**
+     * Get the message headers.
+     */
+    public function headers(): Headers
+    {
+        return new Headers(
+            env('MAIL_FROM_ADDRESS'),
+            [env('MAIL_FROM_ADDRESS')],
+            [
+                'X-Custom-Header' => 'Custom Value',
+            ],
+        );
     }
 }
---
+++ frontend/src/components/SelectLocation.tsx
@@ -0,0 +1,102 @@
+import React, { useState, useEffect, useRef } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  APIProvider,
+  useMap,
+  useMapsLibrary,
+  useAdvancedMarkerRef,
+} from "@vis.gl/react-google-maps";
+import { TextField } from "@mui/material";
+
+const API_KEY = "";
+interface SelectedLcationProps {
+  selectedPlace: google.maps.places.PlaceResult | null;
+  setSelectedPlace: (address: any) => void;
+}
+const SelectLocation = ({
+  selectedPlace,
+  setSelectedPlace,
+}: SelectedLcationProps) => {
+  const [markerRef, marker] = useAdvancedMarkerRef();
+  console.log(selectedPlace);
+
+  return (
+    <APIProvider
+      apiKey={API_KEY}
+      solutionChannel="GMP_devsite_samples_v3_rgmautocomplete"
+    >
+      <div className="autocomplete-control">
+        <PlaceAutocomplete onPlaceSelect={setSelectedPlace} />
+      </div>
+    </APIProvider>
+  );
+};
+
+interface MapHandlerProps {
+  place: google.maps.places.PlaceResult | null;
+  marker: google.maps.marker.AdvancedMarkerElement | null;
+}
+
+const MapHandler = ({ place, marker }: MapHandlerProps) => {
+  const map = useMap();
+
+  useEffect(() => {
+    if (!map || !place || !marker) return;
+
+    if (place.geometry?.viewport) {
+      map.fitBounds(place.geometry?.viewport);
+    }
+    marker.position = place.geometry?.location;
+  }, [map, place, marker]);
+
+  return null;
+};
+
+interface PlaceAutocompleteProps {
+  onPlaceSelect: (place: google.maps.places.PlaceResult | null) => void;
+}
+
+const PlaceAutocomplete = ({ onPlaceSelect }: PlaceAutocompleteProps) => {
+  const [placeAutocomplete, setPlaceAutocomplete] =
+    useState<google.maps.places.Autocomplete | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const places = useMapsLibrary("places");
+
+  useEffect(() => {
+    if (!places || !inputRef.current) return;
+    console.log(inputRef.current.childNodes[1].firstChild);
+    let current = inputRef.current.childNodes[1].firstChild;
+    const options = {
+      fields: ["geometry", "name", "formatted_address"],
+    };
+
+    setPlaceAutocomplete(new places.Autocomplete(current, options));
+  }, [places]);
+
+  useEffect(() => {
+    if (!placeAutocomplete) return;
+
+    placeAutocomplete.addListener("place_changed", () => {
+      onPlaceSelect(placeAutocomplete.getPlace());
+    });
+  }, [onPlaceSelect, placeAutocomplete]);
+
+  return (
+    <div className="autocomplete-container">
+      <TextField
+        id="outlined-basic"
+        label="Location"
+        variant="outlined"
+        autoComplete="off"
+        fullWidth
+        ref={inputRef}
+        inputRef={inputRef}
+      />
+    </div>
+  );
+};
+
+// const root = createRoot(document.getElementById('app')!);
+// root.render(<SelectLocation />);
+
+export default SelectLocation;
